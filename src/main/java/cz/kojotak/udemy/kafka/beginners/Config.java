f70a11d6076412c44e9bfc3e9bd683a26230231a
kojotak <tomas.beneda@gmail.com>
2021-05-24 17:17:11 +0200

    twitter keys revoked

--- src/main/java/cz/kojotak/udemy/kafka/beginners/Config.java
+++ src/main/java/cz/kojotak/udemy/kafka/beginners/Config.java
@@ -4,6 +4,7 @@ public final class Config {
 
 	public static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
 	
+	//tokens are revoked, regenerate from https://developer.twitter.com/en/portal/apps/20939039/keys
 	public static final String TWITTER_API_KEY = "YRwNvbb7rfqZvSc9XJjkyNI2r";
 	public static final String TWITTER_API_SECRET = "qprfXtMCOMmAXpQ9EPi6c1vcVyd17e7CUew1dFZrZmmfdabYGs";
 	public static final String TWITTER_TOKEN = "18406353-99CFGDoZeAsZVWyYrNHz40jTnO07n9tku9UJqpIv8";
