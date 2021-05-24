826e549cbdf4fedc8af3ecce950c7bd206e40839
Tomáš Beneda <tomas.beneda@gmail.com>
2021-05-24 17:35:46 +0200

    Update Config.java
    empty, already revoked, keys and secrets

--- src/main/java/cz/kojotak/udemy/kafka/beginners/Config.java
+++ src/main/java/cz/kojotak/udemy/kafka/beginners/Config.java
@@ -5,9 +5,9 @@ public final class Config {
 	public static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
 	
 	//tokens are revoked, regenerate from https://developer.twitter.com/en/portal/apps/20939039/keys
-	public static final String TWITTER_API_KEY = "YRwNvbb7rfqZvSc9XJjkyNI2r";
-	public static final String TWITTER_API_SECRET = "qprfXtMCOMmAXpQ9EPi6c1vcVyd17e7CUew1dFZrZmmfdabYGs";
-	public static final String TWITTER_TOKEN = "18406353-99CFGDoZeAsZVWyYrNHz40jTnO07n9tku9UJqpIv8";
-	public static final String TWITTER_SECRET = "FDP8XKrtNCaVTmib04TpgKVe3QfVaFJHmgbJa6pNLVJp8";
+	public static final String TWITTER_API_KEY = "";
+	public static final String TWITTER_API_SECRET = "";
+	public static final String TWITTER_TOKEN = "";
+	public static final String TWITTER_SECRET = "";
 
 }
