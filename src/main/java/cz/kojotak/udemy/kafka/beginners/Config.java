89ebe93ed548a85c8adf9a52cbfcd58909dee79b
kojotak <tomas.beneda@gmail.com>
2021-05-27 15:15:40 +0200

    revoked tokens, passwords

--- src/main/java/cz/kojotak/udemy/kafka/beginners/Config.java
+++ src/main/java/cz/kojotak/udemy/kafka/beginners/Config.java
@@ -5,15 +5,16 @@ public final class Config {
 	public static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
 	
 	//tokens are revoked, regenerate from https://developer.twitter.com/en/portal/apps/20939039/keys
-	public static final String TWITTER_API_KEY = "hknQJdhi17Qosq2pLdZW0Be4S";
-	public static final String TWITTER_API_SECRET = "y0X01i5zve6rK5wtWh4L9hv87P4ryCKkKeEVd0DVfNfwe4fx48";
-	public static final String TWITTER_TOKEN = "18406353-QnvTpXmod2dl8ZB7OvWBpq2fTagWyeq5nJLeGwUyX";
-	public static final String TWITTER_SECRET = "YwRlBlNAO6feWO6I6lOeJmORgzSHWMgRJqEYH0F8v0muw";
+	public static final String TWITTER_API_KEY = "";
+	public static final String TWITTER_API_SECRET = "";
+	public static final String TWITTER_TOKEN = "";
+	public static final String TWITTER_SECRET = "";
 	
 	//https://app.bonsai.io/clusters/udemy-kafka-course-9048342856/tokens
-	//https://979136621t:n9dcc30bhw@udemy-kafka-course-9048342856.eu-west-1.bonsaisearch.net:443
-	public static final String BONSAI_HOSTNAME = "udemy-kafka-course-9048342856.eu-west-1.bonsaisearch.net";
-	public static final String BONSAI_USERNAME = "979136621t";
-	public static final String BONSAI_PASSWORD = "n9dcc30bhw";
+	//example (obsolete) url: https://979136621t:n9dcc30bhw@udemy-kafka-course-9048342856.eu-west-1.bonsaisearch.net:443
+	//cluster has been deleted, username and password are no longer valid
+	public static final String BONSAI_HOSTNAME = "";
+	public static final String BONSAI_USERNAME = "";
+	public static final String BONSAI_PASSWORD = "";
 
 }
